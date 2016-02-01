82f7b25ffe7ff3ed52a27cbd9fab0e7ad1164c16
Himani <hplaha@gmail.com>
2016-01-31 22:00:22 -0800

    Initial Commit

---
+++ multiTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiTest
+//
+//  Created by Himani Plaha on 1/31/16.
+//  Copyright © 2016 Jehovah Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
